d2991e0614a0aaafa9abcfcf1fbc751d45474a75
Gökmen Görgen <gkmngrgn@gmail.com>
2020-05-14 00:46:55 +0200

    template rendering is ok. so the next step is to include css files.

--- README.md
+++ README.md
@@ -17,7 +17,6 @@ seg generate --src=templates/src/ \
 
 # send a test mail to preview
 seg send --template=templates/dst/wedding-invitation/ \
-         --subject="You're invited to the wedding Ayse & Mehmet" \
          --email="john.doe@localhost"
 ```
 
@@ -38,6 +37,7 @@ auto-generate it for making your emails more accessible for all human being.
 
 ## TO DO
 
+[ ] multilanguage support.
 [ ] minify css and html contents.
 [ ] embed css files to the email template automatically.
 [ ] support to embed markdown files to the email body as a variable.
--- src/template.rs
+++ src/template.rs
@@ -4,10 +4,73 @@ use tera::{Context, Tera};
 
 use crate::{constants, find_all_templates};
 
-pub fn get_context_data(_template_dir: &PathBuf) -> Context {
-    let mut context = Context::new();
-    context.insert(constants::VAR_SUBJECT, "Hello, world!");
-    context
+struct Email {
+    tera: Tera,
+    src_dir: PathBuf,
+    dst_dir: PathBuf,
+    context_data: Context,
+    subject: String,
+    body: String,
+    body_text: String,
+}
+
+impl Email {
+    fn new(tera: Tera, src_dir: PathBuf, dst_dir: PathBuf) -> Self {
+        Self {
+            tera,
+            src_dir,
+            dst_dir,
+            subject: "".to_string(),
+            body: "".to_string(),
+            body_text: "".to_string(),
+            context_data: Context::new(),
+        }
+    }
+
+    fn add_context_data(&mut self, name: &str, value: &str) {
+        self.context_data.insert(name, value);
+    }
+
+    fn render_template(&mut self, template_name: &str) -> Result<String, String> {
+        let template_name = format!(
+            "{}/{}",
+            self.src_dir.iter().last().unwrap().to_str().unwrap(), // WTF??
+            template_name
+        );
+        match self.tera.render(&template_name, &self.context_data) {
+            Ok(html) => Ok(html),
+            Err(e) => {
+                return Err(format!(
+                    "There are undefined variables in the template. {}",
+                    e
+                ));
+            }
+        }
+    }
+
+    fn render_templates(&mut self) -> Result<(), String> {
+        self.subject = self.render_template(constants::FILE_SUBJECT)?;
+        self.add_context_data(constants::VAR_SUBJECT, self.subject.clone().as_str());
+        self.body = self.render_template(constants::FILE_BODY)?;
+        self.body_text = if self.src_dir.join(constants::FILE_BODY_TEXT).exists() {
+            self.render_template(constants::FILE_BODY_TEXT)?
+        } else {
+            self.strip_tags(self.body.clone().as_str())?
+        };
+        Ok(())
+    }
+
+    fn save_rendered_outputs(self) -> std::io::Result<()> {
+        fs::write(self.dst_dir.join(constants::FILE_SUBJECT), self.subject)?;
+        fs::write(self.dst_dir.join(constants::FILE_BODY), self.body)?;
+        fs::write(self.dst_dir.join(constants::FILE_BODY_TEXT), self.body_text)?;
+        Ok(())
+    }
+
+    fn strip_tags(&mut self, _text: &str) -> Result<String, String> {
+        // TODO: we need to prepare a new function to strip html tags from body.
+        Ok("".to_string())
+    }
 }
 
 pub fn generate_all_templates(src_dir: PathBuf, dst_dir: PathBuf) -> Result<(), String> {
@@ -19,26 +82,21 @@ pub fn generate_all_templates(src_dir: PathBuf, dst_dir: PathBuf) -> Result<(), 
                 return Err(format!("Template directory error: {}", e));
             }
 
-            for template in templates.unwrap() {
-                let template_dir = template.iter().last().unwrap();
-                let output_dir = dst_dir.join(template_dir);
-                match fs::create_dir_all(output_dir) {
+            for template_src in templates.unwrap() {
+                let template_dst = dst_dir.join(template_src.iter().last().unwrap());
+                if let Err(e) = fs::create_dir_all(&template_dst) {
+                    return Err(format!("Error: directory couldn't be created. {}", e));
+                }
+                let mut email = Email::new(tera.clone(), template_src, template_dst);
+                match email.render_templates() {
                     Ok(_) => {
-                        let context_data = get_context_data(&template);
-                        let template_name = format!(
-                            "{}/{}",
-                            template_dir.to_str().unwrap(),
-                            constants::FILE_BODY
-                        );
-                        let output = tera.render(&template_name, &context_data);
-                        println!("DEBUG: {:?}", output);
+                        if let Err(e) = email.save_rendered_outputs() {
+                            return Err(format!("read error: {}", e));
+                        }
                     }
-                    Err(e) => {
-                        return Err(format!("Error: directory couldn't be created. {}", e));
-                    }
+                    Err(e) => return Err(e),
                 }
             }
-
             Ok(())
         }
         Err(e) => Err(format!("Template parsing error: {}", e)),
